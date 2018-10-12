3b99001d20e662b2f052502fb79720364976fda2
İlhan Subaşı <762377+ilhansubasi@users.noreply.github.com>
2018-10-13 00:48:30 +0300

    Unit test

---
+++ src/SimpleStatisticsTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleStatisticsTest {
+
+	@org.junit.jupiter.api.Test
+	void getMinimum() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getMaximum() {
+		double dataSet[] = {8.7, 1.01, 2.1, 5.02};
+		double max = new SimpleStatistics().getMaximum(dataSet);
+		assertEquals(8.7, max);
+	}
+
+	@org.junit.jupiter.api.Test
+	void getRange() {
+		double dataSet[] = {8.7, 1.01, 2.1, 5.02};
+		double range = new SimpleStatistics().getRange(dataSet);
+		assertEquals(7.69, range, 0.0001);
+	}
+
+	@org.junit.jupiter.api.Test
+	void getCount() {
+	}
+}
